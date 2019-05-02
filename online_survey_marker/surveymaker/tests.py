894edcf7ce4b232598c6b9a28017fbb63023c461
thelaua <jiananliu@umass.edu>
2019-05-02 10:56:55 -0400

    Changes To Test.py: 1. Add Comments 2. Create another Test to Check If SurveysView get the right survey lists

--- online_survey_marker/surveymaker/tests.py
+++ online_survey_marker/surveymaker/tests.py
@@ -6,21 +6,29 @@ from django.utils import timezone
 
 class ViewsTests(TestCase):
 
+    # For all Test Cases in ViewsTests
     def setUp(self):
-        
+        # Create One User
         user = MCUser(username="Barry",date_joined=timezone.now())
         user.save()
         self.user = user
 
-    def createSurvey(self,user):
-        #Setup database with one user with one survey with one choice
-        survey = user.survey_set.create(survey_name="Dummy Survey",pub_date=timezone.now())
+    def createUser(self,username="Dummy"):
+        #Create one User
+        user = MCUser(username=username,date_joined=timezone.now())
+        user.save()
+        return user
+
+    def createSurvey(self,user,surveyname="Dummy Survey"):
+        # Create One Survey for user
+        survey = user.survey_set.create(survey_name=surveyname,pub_date=timezone.now())
         survey.save()
 
         return survey
         
-    def createQuestion(self,survey):
-        question = survey.mcquestion_set.create(question_text="Dummy Question")
+    def createQuestion(self,survey,question_text="Dummy Question"):
+        # Create one Question under survey
+        question = survey.mcquestion_set.create(question_text=question_text)
         question.save()
 
         return question
@@ -29,35 +37,43 @@ class ViewsTests(TestCase):
 
         url = reverse('surveymaker:index')
         reponse = self.client.get(url)
-        #reponse contain username
+        #Check If Index Page Retrieve the username
         return self.assertContains(reponse,"Barry")
 
     def test_ssview_url_zero_survey(self):
-
         url = reverse('surveymaker:ssview',args=(self.user.id,))
         reponse = self.client.get(url)
-        #reponse contain place holder text
+        #Test One Check If SurveysView has the right place holder.
+        return self.assertContains(reponse,"Dont Have Any Survey")
+
+    def test_ssview_url_zero_survey_alt(self):
+        # Barry has no Survey but Dummy has one Survey
+        user_dummy = self.createUser(self)
+        self.createSurvey(user_dummy)
+        url = reverse('surveymaker:ssview',args=(self.user.id,))
+        reponse = self.client.get(url)
+        #Test Two Check If SurveysView has the right place holder.
         return self.assertContains(reponse,"Dont Have Any Survey")
 
     def test_ssview_url_one_survey(self):
         self.createSurvey(user = self.user)
         url = reverse('surveymaker:ssview',args=(self.user.id,))
         reponse = self.client.get(url)
-        #reponse contain place holder text
-        return self.assertNotContains(reponse,"Dont Have Any Survey")
+        #Check If SurveysView has the right Survey
+        return self.assertContains(reponse,"Dummy Survey")
 
     def test_ssview_url_one_question(self):
         survey = self.createSurvey(user = self.user)
         self.createQuestion(survey = survey)
         url = reverse('surveymaker:sview',args=(survey.id,))
         reponse = self.client.get(url)
-        #reponse contain place holder text
+        #Check If SurveyView has the right Question
         return self.assertContains(reponse,"Dummy Question")
 
     def test_sview_url_invalid(self):
+        # Simple Test for 404 response
         url = reverse('surveymaker:sview',args=(20,))
         reponse = self.client.get(url)
-        #reponse contain place holder text
         return self.assertEqual(reponse.status_code,404)
 
 
