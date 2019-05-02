704b2a41474d888d717551e88f5b54633bff4f10
thelaua <jiananliu@umass.edu>
2019-05-01 22:34:28 -0400

    Setup initial test cases

--- online_survey_marker/surveymaker/tests.py
+++ online_survey_marker/surveymaker/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+from .models import MCUser,MCQuestion, MCChoice, Survey
+from django.utils import timezone
+class ViewTests(TestCase):
+
+    def setUp(self):
+        user = MCUser(username="Barry",date_joined=timezone.now())
+        user.save()
+
+    def test_index_url(self):
+        url = reverse('surveymaker:index')
+        reponse = self.client.get(url)
+        #Should return one user
+        return self.assertContains(reponse,"Barry")
